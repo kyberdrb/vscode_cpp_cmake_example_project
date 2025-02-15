e68467b8642bd1a79c52a412aceac5616cd82d06
kyberdrb <kyberdrb@gmail.com>
2025-02-16 00:25:29 +0100

    Add some C++20/23 features that the clangd red squiggly underlines

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <print>
 
 int main() {
     int number = 42;
     std::cout << "Hello, VS Code + CMake + clangd!" << std::endl;
     std::cout << " " << number << std::endl;
+    std::cout << std::format("Testing std::format") << '\n';
+    std::print("Testing std::print");
     return 0;
 }

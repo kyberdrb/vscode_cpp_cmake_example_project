31693f5f0be8a44ea9b14198891151ff20b4ba1b
kyberdrb <kyberdrb@gmail.com>
2025-02-19 13:11:06 +0100

    Add lambda predicate example to check for presence of element + Divide output into sections

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <expected>
 #include <iostream>
 #include <print>
 #include <source_location>
+#include <vector>
 
 // constexpr functions need to be defined before usage - forward declaration is not enough - otherwise compiler throws an error:
 //   error: ‘constexpr std::expected<unsigned int, std::__cxx11::basic_string<char> > triggerArtificialException(bool)’ used before its definition
@@ -26,15 +28,32 @@ int main() {
     std::cout << std::format("Testing std::format") << '\n';
     std::print("Testing std::print\n");
 
+    std::println("");
+
     constexpr auto resultWithValue = triggerArtificialException(false);
     if (resultWithValue.has_value()) {
         std::println("triggerArtificialException(false) [happy path]: {}", resultWithValue.value());
     }
 
+    std::println("");
+
     auto resultWithException = triggerArtificialException(true);
     bool hasException = !(resultWithException.has_value());
     if (hasException) {
         std::println("triggerArtificialException(true) [catching exception]: {}", resultWithException.error());
+    }
+
+    std::println("");
+
+    std::vector numbers{1, 2, 3};
+
+    auto isElementFound = std::any_of(numbers.begin(), numbers.end(), 
+        [](auto currentNumber){
+            return currentNumber == 2;
+        } );
+
+    if (isElementFound) {
+        std::println("std::any_of found an element matching criteria");
     }
 
     return 0;

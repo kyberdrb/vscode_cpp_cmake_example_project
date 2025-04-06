0fd0f232d0b71c3526b78cb2e8ea421fa1ff0c21
kyberdrb <kyberdrb@gmail.com>
2025-04-06 18:47:20 +0200

    Remove FluentBuilder altogether + make 'number' var 'static constexpr'

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
-#include "FluentBuilder.h"
-
-#include <chrono>
-#include <iostream>
-#include <print>
-
 #include <algorithm>
+#include <chrono>
 #include <expected>
 #include <iostream>
 #include <print>
@@ -28,7 +23,7 @@ constexpr std::expected<uint32_t, std::string> triggerArtificialException(bool s
 }
 
 int main() {
-    constexpr int number = 42;
+    static constexpr int number = 42;
     std::cout << "Hello, VS Code + CMake + clangd!" << std::endl;
     std::cout << number << std::endl;
     std::cout << std::format("Testing std::format") << '\n';
@@ -61,59 +56,6 @@ int main() {
     if (isElementFound) {
         std::println("std::any_of found an element matching criteria");
     }
-
-    std::println("");
-
-    auto somethingFluentToBuild = FluentBuilder::create()
-        .withNumber(2)
-        .withText("Stack Builder is awesome");
-
-    // TODO make Builder creation constexpr
-    // constexpr auto somethingFluentToBuild = FluentBuilder::create()
-    //     .withNumber(2)
-    //     .withText("Stack Builder is awesome");
-
-    auto start = std::chrono::steady_clock::now();
-    std::cout << "std::cout: " << somethingFluentToBuild << '\n';
-    auto end = std::chrono::steady_clock::now();
-    auto executionTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-    std::println("std::cout (execution time): {}", executionTime);
-
-    start = std::chrono::steady_clock::now();
-    std::println("std::print: {}", somethingFluentToBuild.str());
-    end = std::chrono::steady_clock::now();
-    executionTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-    std::println("std::print (execution time): {}", executionTime);
-
-    std::println("");
-
-    // Compiles, but segfaults
-    // auto somethingUniquelyFluentToBuild = FluentBuilder::createUnique();
-    // auto wup = std::move(somethingUniquelyFluentToBuild)
-    //         ->addSomethingUnique(4)
-    //         ->addSomethingElseUnique("Builder is awesome");
-
-    // Doesn't compile: operator '->' discards 'rvalue' qualifiers transforming 'rvalue' refs into 'lvalue' refs
-    //  Returning unique_ptr is valid, but chaining unique_ptr pointers is ill-formed, as it requires moving ownership back and forth while requiring owning the instance by the client and the built instance at the same time, which would require double deletion
-    // auto somethingUniquelyFluentToBuild = std::move(FluentBuilder::createUnique());
-    //         ->addSomethingUnique(4)
-    //         ->addSomethingElseUnique("Builder is awesome");
-
-    auto somethingUniquelyFluentToBuild = FluentBuilder::createUnique() // auto will first start with unique_ptr...
-        ->withUniqueNumber(4) // ...then will convert to lvalue reference...
-        .withUniqueText("Unique Builder is awesome"); // ...and stays as lvalue reference
-
-    start = std::chrono::steady_clock::now();
-    std::cout << "std::cout: " << somethingUniquelyFluentToBuild << '\n';
-    end = std::chrono::steady_clock::now();
-    executionTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-    std::println("std::cout (execution time): {}", executionTime);
-
-    start = std::chrono::steady_clock::now();
-    std::println("std::print: {}", somethingUniquelyFluentToBuild.str());
-    end = std::chrono::steady_clock::now();
-    executionTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-    std::println("std::print (execution time): {}", executionTime);
 
     std::println();
 
